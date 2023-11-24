e1519c47e960d5fdf7dee1e20d0a507e4c786664
Nerijus Padleckis <padleckis.nerijus@gmail.com>
2023-11-24 21:07:20 +0200

    pirmas edit

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
                         <span className="icon-bar"></span>{" "}
                     </button>
                     <a className="navbar-brand page-scroll" href="#page-top">
-                        React Landing Page
+                        MEDITACIJOS NEKTARAS
                     </a>{" "}
                 </div>
 
@@ -29,37 +29,37 @@ export const Navigation = (props) => {
                     <ul className="nav navbar-nav navbar-right">
                         <li>
                             <a href="#features" className="page-scroll">
-                                Features
+                                Savybės
                             </a>
                         </li>
                         <li>
                             <a href="#about" className="page-scroll">
-                                About
+                                Apie Mus
                             </a>
                         </li>
-                        <li>
-                            <a href="#services" className="page-scroll">
-                                Services
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#portfolio" className="page-scroll">
-                                Gallery
-                            </a>
-                        </li>
+                        {/*<li>*/}
+                        {/*    <a href="#services" className="page-scroll">*/}
+                        {/*        Services*/}
+                        {/*    </a>*/}
+                        {/*</li>*/}
+                        {/*<li>*/}
+                        {/*    <a href="#portfolio" className="page-scroll">*/}
+                        {/*        Gallery*/}
+                        {/*    </a>*/}
+                        {/*</li>*/}
                         <li>
                             <a href="#testimonials" className="page-scroll">
-                                Testimonials
+                                Atsiliepimai
                             </a>
                         </li>
                         <li>
                             <a href="#team" className="page-scroll">
-                                Team
+                                Komanda
                             </a>
                         </li>
                         <li>
                             <a href="#contact" className="page-scroll">
-                                Contact
+                                Kontaktai
                             </a>
                         </li>
                     </ul>
