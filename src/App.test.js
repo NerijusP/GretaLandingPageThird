20dd75bf9dab23cb92aba1b679a52fdad0960da4
Nerijus Padleckis <padleckis.nerijus@gmail.com>
2023-11-24 21:15:30 +0200

    test Meditacijos add

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/REACT LANDING PAGE/i);
+    const linkElement = screen.getByText(/MEDITACIJOS/i);
     expect(linkElement).toBeInTheDocument();
 });
